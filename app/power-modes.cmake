118daad5a1f44dfee31695a6218a5b9e2471ab79
Volodymyr Medvid <vmedvid@riseup.net>
2020-12-17 20:18:07 +0000

    app: add mtb-example-psoc4-power-modes

---
+++ app/power-modes.cmake
@@ -0,0 +1,19 @@
+project(power-modes)
+
+# This application provides custom design.modus for the below boards
+psoc4_check_bsp(
+  CY8CKIT-041-41XX
+  CY8CKIT-145-40XX
+  CY8CKIT-149
+)
+
+psoc4_load_application(
+  NAME mtb-example-psoc4-power-modes
+  VERSION 1.0.0
+)
+psoc4_add_executable(
+  SOURCES
+    ${APP_DIR}/main.c
+  DESIGN_MODUS
+    ${APP_DIR}/COMPONENT_CUSTOM_DESIGN_MODUS/TARGET_${BSP_NAME}/design.modus
+)
